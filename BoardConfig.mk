61c9bf8e741c7f353bb3bddba6bc8fd2525fec8e
Lunarixus <Nathanb644@hotmail.co.uk>
2020-07-26 02:10:07 +0000

    tulip: Specify that we use AOSP surfaceflinger
    Signed-off-by: Lunarixus <Nathanb644@hotmail.co.uk>
    Signed-off-by: AtlanPrime <zilliondollarkuruvi@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -132,6 +132,9 @@ TARGET_USES_HWC2 := true
 TARGET_USES_GRALLOC1 := true
 TARGET_USES_ION := true
 
+# Display- surfaceflinger
+TARGET_USES_AOSP_SURFACEFLINGER := true
+
 # DRM
 TARGET_ENABLE_MEDIADRM_64 := true
 
