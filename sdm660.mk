59d29ac9b51ac65fb559465e714eec560a2e778a
AtlanPrime <zilliondollarkuruvi@gmail.com>
2020-10-28 04:44:26 +0000

    sdm660-common: disable XiaomiDoze and XiaomiParts *temp

--- sdm660.mk
+++ sdm660.mk
@@ -212,10 +212,6 @@ PRODUCT_PACKAGES += \
     vendor.display.config@1.9 \
     vendor.display.config@1.9_vendor
 
-# Doze
-PRODUCT_PACKAGES += \
-    XiaomiDoze
-
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
@@ -311,7 +307,6 @@ PRODUCT_PACKAGES += \
     init.qcom.usb.sh \
     init.recovery.qcom.rc \
     init.target.rc \
-    init.xiaomi_parts.rc \
     init.performance.sdm660.rc \
     ueventd.qcom.rc \
     init.safailnet.rc
@@ -621,7 +616,3 @@ PRODUCT_PACKAGES += \
 # WiFi Display JAR
 PRODUCT_BOOT_JARS += \
     WfdCommon
-
-# XiaomiParts
-PRODUCT_PACKAGES += \
-   XiaomiParts
