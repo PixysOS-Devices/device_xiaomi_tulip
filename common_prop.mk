b077c7ea6395b5bd3ae8632a1b03149b415cbe1e
Astile <zilliondollarkuruvi@gmail.com>
2020-10-29 07:45:31 +0530

    sdm660-common: Update to Coral's Oct 2020 fingerprint

--- common_prop.mk
+++ common_prop.mk
@@ -1,5 +1,5 @@
 #Build FP to be picked by both system and vendor
-BUILD_FINGERPRINT := "xiaomi/wayne/wayne:8.1.0/OPM1.171019.011/V9.5.11.0.ODCCNFA:user/release-keys"
+BUILD_FINGERPRINT := "google/coral/coral:11/RP1A.201005.004/6782484:user/release-keys"
 
 # ART
 PRODUCT_PROPERTY_OVERRIDES += \
