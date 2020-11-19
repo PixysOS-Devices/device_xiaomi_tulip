9c345e6747b02d14408c2fbdb0bef124bcfa6d87
AtlanPrime <zilliondollarkuruvi@gmail.com>
2020-11-19 09:01:54 +0000

    tulip: remove  basic legacy usb hal * xiaomi_sdm660 one is already defined

--- device.mk
+++ device.mk
@@ -498,10 +498,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     init.device.rc
 
-# USB
-PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service.basic
-
 # Watermark
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/media/MIUI_DualCamera_watermark.png:$(TARGET_COPY_OUT_VENDOR)/etc/MIUI_DualCamera_watermark.png
